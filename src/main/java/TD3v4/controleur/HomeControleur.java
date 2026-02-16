b2426aa2bc03d427c914bbee4944f2ca309b8cb0
Florian Chevalier <f-chevalier@efluid.fr>
2026-02-16 11:27:47 +0100

    Ajout d'une redirection depuis la page d'accueil, vers la page "étudiants".

---
+++ src/main/java/TD3v4/controleur/HomeControleur.java
@@ -0,0 +1,13 @@
+package TD3v4.controleur;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeControleur {
+
+  @GetMapping("/")
+  public String redirection() {
+    return "redirect:/etudiants";
+  }
+}
